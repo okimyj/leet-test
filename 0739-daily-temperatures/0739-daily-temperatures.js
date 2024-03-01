45a51ca853b7fc14484530f7d655a7e44f7cf3bc
okimyj <okimyj@gmail.com>
2024-03-01 15:04:29 +0900

    Time: 209 ms (71.11%), Space: 73.9 MB (38.25%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+  const output = Array(temperatures.length).fill(0);
+  const stack = [];
+  for(let i =0; i<temperatures.length; ++i){
+    while(stack.length > 0 && stack[stack.length-1].value < temperatures[i]){
+      const data = stack.pop();
+      output[data.day] = i - data.day;  
+    }
+    stack.push({day:i, value:temperatures[i]})
+  }
+  return output;
+};
