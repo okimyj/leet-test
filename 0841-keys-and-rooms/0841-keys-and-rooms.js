e2d440276ff1e10331225019a2afd1cb2a077c05
okimyj <okimyj@gmail.com>
2024-03-02 00:50:14 +0900

    Time: 57 ms (61.14%), Space: 50.6 MB (59.50%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.js
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -3,6 +3,29 @@
  * @return {boolean}
  */
 var canVisitAllRooms = function(rooms) {
+    const visited = {0:true}
+    const reserve = [0]
+    let visitedNum = 1;
+    while(reserve.length > 0){
+        const curKey = reserve.pop();        // 얘는 pop 해도 상관 없을 듯?..
+        for(let i=0; i<rooms[curKey].length; ++i){
+            const newKey = rooms[curKey][i];
+            
+            if(!visited[newKey]){
+                reserve.push(newKey)
+                visited[newKey] = true;
+                ++visitedNum;
+            }
+        }
+        if(visitedNum === rooms.length)
+            break;
+        
+    }
+    return visitedNum === rooms.length;
+}
+/*
+// dfs
+var canVisitAllRooms = function(rooms) {
     if(!rooms || rooms.length <= 0)
         return false;
     const visited = [true];
@@ -21,4 +44,4 @@ var canVisitAllRooms = function(rooms) {
     dfs(rooms[0])
     
     return visitedNum === rooms.length;
-};
+};*/
