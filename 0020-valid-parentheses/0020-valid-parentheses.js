7b390f4df6130a9a50507c6898643ea3bacd10c2
okimyj <okimyj@gmail.com>
2024-03-01 14:37:40 +0900

    Time: 67 ms (17.29%), Space: 50.2 MB (39.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -8,17 +8,15 @@ const isMatch = function(open, close){
 var isValid = function(s) {
     const opens = "({["
     const stack = []
-    let check = true;
     for(let i=0; i<s.length; ++i){    // O(n)
       if(opens.includes(s[i]))
         stack.push(s[i]);   // O(1)
       else{
         if(stack.length <= 0 || !isMatch(stack.pop(), s[i])){   // O(1)
-          check = false;
-          break;
+          return false
         }
       }
     }
-    return check && stack.length <= 0;
+    return stack.length <= 0;
 };
 // 시간 복잡도 : O(n)
